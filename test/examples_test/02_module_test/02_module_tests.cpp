b6fdb38c7d19eee0bf91285da2be022cdc5be400
Archer <graham.archer@g.austincc.edu>
2019-09-12 20:56:16 -0500

    zzxzzzz

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,4 +1,4 @@
-CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
