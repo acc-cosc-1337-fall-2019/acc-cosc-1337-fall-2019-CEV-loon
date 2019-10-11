29c622ca38e34fe4c1c0e17fd24e6d9794397721
Archer <graham.archer@g.austincc.edu>
2019-10-10 21:57:24 -0500

    incomplete hw 10-10-19

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,64 @@
 //cpp
+#include"tic_tac_toe.h"
+bool tictactoe::game_over()
+{
+	return false;
+}
+void tictactoe::start_game(string player)
+{
+	next_player = player;
+	clear_bord();
+}
+void tictactoe::mark_board(int position)
+{
+
+}
+string tictactoe::get_player() const
+{
+	return next_player;
+}
+void tictactoe::display_board() const
+{
+	//iterate vector and display to screen
+}
+//define function
+void tictactoe::set_next_player()
+{
+	if (next_player == "x")
+	{
+		next_player = "o";
+	}
+	else
+	{
+		next_player = "x";
+	}
+
+}
+
+bool tictactoe::check_row_win()
+{
+}
+
+bool tictactoe::check_diagonal_win()
+{
+}
+
+bool tictactoe::check_columb_win()
+{
+	for (std::size_t i = 0; i < 3; i++)
+	{
+		if (pegs[i]) == pegs[i + 3] && pegs[i + 3] == pegs[i + 6])
+		{
+		return true;
+}
+	}
+	return false;
+}
+void tictactoe::clear_bord()
+{
+}
+
+bool tictactoe::check_board_full()
+{
+	return false;
+}
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,28 @@
 //h
+//represents one game
+#include<vector>
+#include<string>
+#include<iostream>
+
+
+using std::string; using std::vector;
+
+class tictactoe
+{
+public:
+	bool game_over();
+	void start_game(string player);
+	void mark_board(int position);
+	string get_player() const;
+	void display_board() const;
+
+private:
+	void set_next_player();
+	bool check_columb_win();
+	bool check_row_win();
+	bool check_diagonal_win();
+	void clear_bord();// for ranged or 
+	bool check_board_full();
+	vector<string> pegs(9, " ");
+	string next_player;
+};
