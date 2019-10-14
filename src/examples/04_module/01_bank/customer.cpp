b3616bcaa5bd919de30beca4c7b61fc958cd2f67
8584024 <8584024@RRC2220-19>
2019-10-14 18:13:32 -0500

    class 1 week 7

--- src/examples/04_module/01_bank/customer.cpp
+++ src/examples/04_module/01_bank/customer.cpp
@@ -1,1 +1,20 @@
+#include "customer.h"
+
 //customer.cpp
+
+void customer::add_account(bank_account act)
+{
+	total_balance += act.get_balance();
+	account.push_back(act);
+
+}
+
+ostream & operator<<(ostream & out, const customer& act)
+{
+	out << "customer accounts: /n";
+	for (auto act : c.accounts)
+	{
+		out << act;
+	}
+	out << "/n total balance:" << c.total_balance;
+}
--- src/examples/04_module/01_bank/customer.h
+++ src/examples/04_module/01_bank/customer.h
@@ -1,1 +1,26 @@
 //customer.h
+#include<vector>
+using std::cout;
+using std::<vector>;
+#include "bank_account.h"
+
+
+
+
+
+
+class customer
+{
+	public
+		void add_account(bank_account act);
+	friend ostream& operator<<(ostream& out,
+		const customer& c);
+
+
+
+		private
+		vector<customer> accounts;
+		int total_balance{ 0 };
+
+
+};
