842717c4078ac083012a38e938b1bcc60af7d087
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-11-10 21:12:57 -0600

    HW 10 n 11 grading

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,24 +1,25 @@
 //h
 #ifndef TIC_TAC_TOE_MANAGER_H
 #define TIC_TAC_TOE_MANAGER_H
-
-#include "tic_tac_toe.h"
-#include<vector>
-#include<iostream>
 #include<memory>
+#include<vector>
+#include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
 
+using std::vector;
 using std::unique_ptr;
 using std::ostream;
-using std::vector;
 
 class TicTacToeManager
 {
 public:
-	const std::vector<std::unique_ptr<TicTacToeBoard>>& get_games();
 	void save_game(unique_ptr<TicTacToe>& game);
+	const std::vector<std::unique_ptr<TicTacToe>>& get_games();
 	void get_winner_totals(int& x, int& o, int& c);
-	std::unique_ptr <TicTacToe> get_GAME(int Game_type);
+	unique_ptr<TicTacToe> get_game(int game_type);
 	friend ostream& operator<<(ostream& out, const TicTacToeManager& m);
+
 private:
 	void update_winner_count(string winner);
 	vector<unique_ptr<TicTacToe>> games;
@@ -26,5 +27,6 @@ private:
 	int o_win{ 0 };
 	int ties{ 0 };
 };
+
 
 #endif // !TIC_TAC_TOE_MANAGER_H
