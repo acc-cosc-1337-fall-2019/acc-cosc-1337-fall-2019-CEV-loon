ed778bdf204e5cd72ee3736333833e0fb6c7c7f7
8584024 <8584024@RRC2220-19>
2019-11-18 19:04:57 -0600

    class 11-18 #2

--- src/examples/11_module/11_module/04_dangling_pointer/dangling_pointer.cpp
+++ src/examples/11_module/11_module/04_dangling_pointer/dangling_pointer.cpp
@@ -1,8 +1,13 @@
 #include "dangling_pointer.h"
 
 //
-
+using std::cout;
 void dangling_pointer()
 {
-	
+	int * ptr_num = new_int(5);
+	int * ptr = ptr_num;
+	delete ptr_num;
+	ptr_num = nullptr;
+	cout << *ptr;
+	return 0;
 }
