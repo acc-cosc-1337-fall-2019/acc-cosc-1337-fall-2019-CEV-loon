5b59ed89b33c27e37ab58d7f64376823bccb7dd9
8584024 <8584024@RRC2220-19>
2019-11-18 18:27:17 -0600

    class 11-18

--- src/examples/11_module/11_module/01_ref_pointers/ref_pointers.cpp
+++ src/examples/11_module/11_module/01_ref_pointers/ref_pointers.cpp
@@ -1,9 +1,10 @@
 #include "ref_pointers.h"
 
-//
+//show ref aND POINTER CANN MODIFY DATA POINTS
 
 void ref(int &number, int* numberPtr)
 {
-	
+	number = 500;
+	*numberptr = 1000;
 }
 
--- test/examples_test/11_module_test/11_module_tests.cpp
+++ test/examples_test/11_module_test/11_module_tests.cpp
@@ -1,7 +1,22 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-
+#include"ref_pointer.ccp"
+#include"ref_pointer.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE(" ref and ptr")
+{
+
+	int num1 = 10;
+	int num2 = 20;
+
+	int& numref = num1;
+	int* numptr = &num2;
+
+	ref(numref, numptr);
+	REQUIRE(num1 == 500);
+	REQUIRE(num2 == 1000);
+
+}
