b0779bd0b7bb805f5b89f650bfa3affb2e0faf75
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-11-25 17:35:53 -0600

    Update vector.h

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,19 +1,16 @@
-//
-class Vector
+class Vector 
 {
 public:
 	Vector(size_t sz);
-	Vector(const vector7 v);
-	Vector& operator=(const Vector& v);
-	Vector(const Vector&& v);// move constructor
-	Vector& operator=(Vector&& v);
-	size_t size()const{return size;})
-	int size()const { return size; }
-	int& operator[](int i) { return nums[i]: }
-	int& operator[](int i) { return nums[i]: }
+	Vector(const Vector& v);//copy constructor
+	Vector& operator=(const Vector& v);//copy assignment
+	Vector(Vector&& v);//move constructor
+	Vector& operator=(Vector&& v);//move assignment
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i) const { return nums[i]; }
 	~Vector();
 private:
 	size_t size;
 	int* nums;
-
 };
